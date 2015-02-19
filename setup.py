b7a6377015dd4f808315900b9688444e4dcde86c
Stuart Axon <stuaxo2@yahoo.com>
2015-02-19 15:17:12 +0700

    Doh, make version the same as vext

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    version='0.1.1',
 
     description='Use system python packages in a virtualenv',
     long_description=long_description,
