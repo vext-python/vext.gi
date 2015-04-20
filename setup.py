bdb17d616602df1495f2e81cbb2be53eed7cb455
Stuart Axon <stuaxo2@yahoo.com>
2015-04-21 00:43:02 +0700

    Add cairo to allowed modules, to make PangoCairo work.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
 
     description='Use system python packages in a virtualenv',
     long_description=long_description,
@@ -56,15 +56,11 @@ setup(
     # What does your project relate to?
     keywords='virtualenv gi vext gtk',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    #packages=['vext.gi'],
-
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["vext>=0.1.1"],
+    install_requires=["vext>=0.3.0"],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
