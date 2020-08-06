3e6aafdf6f01b3c1dfb27426ca1e81c9f69999a9
Stuart Axon <stuaxo2@yahoo.com>
2020-08-06 15:02:46 +0100

    Bump version in setup.py and format with Black.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from __future__ import print_function
 
-info="""
+info = """
 Allow use of system gi.repository (Gtk3) from a virtualenv
 Should work on all platforms.
 
 report bugs to https://github.com/stuaxo/vext
 """
 
-version="0.7.4"
-vext_version="vext>=%s" % version
+version = "0.7.4"
+vext_version = "vext>=%s" % version
 
 
 from glob import glob
@@ -38,63 +38,63 @@ class Install(install):
         #
         # TOOD - Move this code to a common place.
         print("vext.gi Install")
-        if sys.prefix == '/usr':
+        if sys.prefix == "/usr":
             print("Not installing PTH file to real prefix")
             return
 
         install.run(self)
         install.execute(self, _post_install, [self], msg="Install vext files:")
 
+
+# Disable wheels
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(_bdist_wheel):
+        def run(self):
+            return
+
+
+except ImportError:
+    bdist_wheel = None
+
+
 setup(
-    name='vext.gi',
+    name="vext.gi",
     zip_safe=False,
-
-    cmdclass={
-          'install': Install,
-    },
-
+    cmdclass={"install": Install,},
     version=version,
-
-    description='Use system python packages in a virtualenv',
+    description="Use system python packages in a virtualenv",
     long_description=info,
-
     # The project's main homepage.
-    url='https://github.com/stuaxo/vext',
-
+    url="https://github.com/stuaxo/vext",
     # Author details
-    author='Stuart Axon',
-    author_email='stuaxo2@yahoo.com',
-
+    author="Stuart Axon",
+    author_email="stuaxo2@yahoo.com",
     # Choose your license
-    license='MIT',
-
+    license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
     ],
-
     # What does your project relate to?
-    keywords='virtualenv gi vext gtk',
-
+    keywords="virtualenv gi vext gtk",
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     setup_requires=["setuptools>=0.18.8"],
     install_requires=[vext_version],
-    data_files=[('share/vext/specs', ["gi.vext"])]
+    data_files=[("share/vext/specs", vext_files)],
 )
